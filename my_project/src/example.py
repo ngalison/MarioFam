60b03f85992230f823806acb98e9d960697b0147
Yash <Yash@ZEN305>
2017-11-14 15:58:26 -0800

    trying to get requests to work

--- my_project/src/example.py
+++ my_project/src/example.py
@@ -3,8 +3,12 @@ Created on Nov 6, 2017
 
 @author: joely
 '''
+import json
+
 i = "hello"
 if (i == "hello"):
     print(i)
 print("AVLTree is evil")
 print("meow meow meow MEOW")
+
+response = request("http://api.open-notify.org/iss-now.json")
