48b63455fb29ddae9cca0fc0d3ec354c90b35082
Egg0 <marcuslr@live.com>
2017-11-21 11:29:55 -0800

    Successfully pulling/sorting data from test api pull

--- my_project/src/mapillary.py
+++ my_project/src/mapillary.py
@@ -6,4 +6,9 @@ Created on Nov 21, 2017
 import requests
 
 boundingbox = requests.get("https://a.mapillary.com/v3/images/?lookat=12.9981086701,55.6075236275&bbox=12.9981086701,55.6075236275,13.0006076843,55.6089295863&client_id=TG1sUUxGQlBiYWx2V05NM0pQNUVMQTo2NTU3NTBiNTk1NzM1Y2U2")
-print(boundingbox)
+print(boundingbox.status_code)
+
+json = boundingbox.json()
+print(json)
+for el in json['features'] :
+    print(el['properties'])
