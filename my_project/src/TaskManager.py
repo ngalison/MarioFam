a4394b7454c0ec2927728c3c43624cf7bc6c5ed3
shahy <shahy@localhost.localdomain>
2017-11-28 15:47:36 -0800

    fixed bug related to null fields

--- my_project/src/TaskManager.py
+++ my_project/src/TaskManager.py
@@ -16,16 +16,18 @@ for word in sorted(coordinates.keys()):
     print(word)
 
 userInput = '' 
-distance = 0.0000   
-try:
-    userInput = coordinates[input('Enter the building you are closest to: ')]
-except KeyError:
-    print('Bad Input: Check for spelling or building eligibility')
+distance = 0.0000
+while(len(userInput) == 0):
+	try:
+	    	userInput = coordinates[input('Enter the building you are closest to: ')]
+	except KeyError:
+		    print('Bad Input: Check for spelling or building eligibility')
 
-try:
-    distance = float(input('How far are you willing to walk in terms of miles? Just enter the number. (ex: 0.1, 1.0. 2.0): '))
-except ValueError:
-    print('Bad Input: Enter a numeric value')
+while(distance == 0):
+	try:
+    		distance = float(input('How far are you willing to walk in terms of miles? Just enter the number. (ex: 0.1, 1.0. 2.0): '))
+	except ValueError:
+    		print('Bad Input: Enter a numeric value')
    
 print(userInput)
 
