b5f6ee939f60c7eb4f1bd841f976d509c53b0385
Joelynuw <joelyn@uw.edu>
2017-12-05 12:36:01 -0800

    working with osm updates

--- my_project/src/WorkingWithOSM.py
+++ my_project/src/WorkingWithOSM.py
@@ -2,5 +2,14 @@ from osmapi import OsmApi
 import overpy
 import overpass
 
-MyApi = OsmApi();
-print (MyApi.NodeGet(4119542984));
+
+def returnFootpaths(slat, slon, nlat, nlon):
+	api = overpy.Overpass()	
+	result = api.query("node(" + str(slat) + "," + str(slon) +"," + str(nlat) + "," + str(nlon) + "); out;")
+	##result = api.query("node(47.596238,-122.34512, 47.633268, -122.27097); out;")
+	
+	return len(result.nodes)
+		
+print(returnFootpaths(47.596238,-122.34512, 47.633268, -122.27097))
+
+
