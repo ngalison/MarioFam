6fff1b55263d397088471aa184aadf0b4cb43646
shahy <shahy@localhost.localdomain>
2017-12-05 12:38:08 -0800

    merging

--- my_project/src/WorkingWithOSM.py
+++ my_project/src/WorkingWithOSM.py
@@ -2,5 +2,8 @@ from osmapi import OsmApi
 import overpy
 import overpass
 
-MyApi = OsmApi();
-print (MyApi.NodeGet(4119542984));
+OSMapi = OsmApi();
+OVERPASSapi = overpy.Overpass();
+#print (MyApi.NodeGet(4119542984));
+points = OVERPASSapi.query("node(50.745,7.17,50.75,7.18);out;")
+print(len(points.nodes))
