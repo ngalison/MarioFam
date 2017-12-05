d9cbe57b7cb01e4d9747010797b168586df50567
Joelynuw <joelyn@uw.edu>
2017-12-05 13:02:14 -0800

    making faster, looking at way properties

--- my_project/src/WorkingWithOSM.py
+++ my_project/src/WorkingWithOSM.py
@@ -5,8 +5,8 @@ import overpass
 
 def returnFootpaths(slat, slon, nlat, nlon):
 	api = overpy.Overpass()	
-	result = api.query("[bbox: " + str(slat) +"," + str(slon) +"," + str(nlat) + "," + str(nlon) +"]; ( node[highway=footway]; node[highway=pedestrian]; node[foot=yes]; node[footway=sidewalk]; way[highway=footway]; way[highway=pedestrian]; way[foot=yes]; way[footway=sidewalk] ); out;")
-	return len(result.ways)
+	result = api.query("[bbox: " + str(slat) +"," + str(slon) +"," + str(nlat) + "," + str(nlon) +"]; (way[highway=footway]; way[highway=pedestrian]; way[foot=yes]; way[footway=sidewalk] ); out;")
+	return result.ways[0]
 		
 print(returnFootpaths(47.596238,-122.34512, 47.633268, -122.27097))
 
