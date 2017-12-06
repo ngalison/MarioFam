bcbf18da8bba4da26c903933d1e9ae3cb918f334
Egg0 <marcuslr@live.com>
2017-12-05 16:06:54 -0800

    Printing querystring

--- my_project/src/WorkingWithOSM.py
+++ my_project/src/WorkingWithOSM.py
@@ -5,9 +5,12 @@ import overpass
 
 def returnFootpaths(slat, slon, nlat, nlon):
 	api = overpy.Overpass()	
-	result = api.query("[bbox: " + str(slat) +"," + str(slon) +"," + str(nlat) + "," + str(nlon) +"]; (way[highway=footway]; way[highway=pedestrian]; way[foot=yes]; way[footway=sidewalk] ); out;")
-	return result.ways[0]
+	queryStr = "[bbox: " + str(slat) +"," + str(slon) +"," + str(nlat) + "," + str(nlon) + "]; (way[highway=footway]; way[highway=pedestrian]; way[foot=yes]; way[footway=sidewalk] ); out;"
+	print (queryStr)
+	result = api.query(queryStr)
+	return result.ways
 		
-print(returnFootpaths(47.596238,-122.34512, 47.633268, -122.27097))
+ways = returnFootpaths(47.596238,-122.34512, 47.633268, -122.27097)
+print (ways[0])
 
 
