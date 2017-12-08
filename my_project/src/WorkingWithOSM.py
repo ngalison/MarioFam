396515dc7ac984d968400bb3affeae1c97d7767c
shahy <shahy@localhost.localdomain>
2017-12-08 15:33:16 -0800

    did osm stuff

--- my_project/src/WorkingWithOSM.py
+++ my_project/src/WorkingWithOSM.py
@@ -6,8 +6,17 @@ import overpass
 def returnFootpaths(slat, slon, nlat, nlon):
 	api = overpy.Overpass()	
 	result = api.query(" [bbox: " + str(slat) +", " + str(slon) + ", " + str(nlat) + ", " + str(nlon) + "]; (way[highway=footway]; way[highway=pedestrian]; way[foot=yes]; way[footway=sidewalk] ); /*added by auto repair*/ (._;>;); /*end of auto repair*/ out;")
-	return result.ways
+	footpaths = result.ways
+	for way in footpaths:
+		print("{")
+		print("\"type\": \"Feature\",")
+		print("\"geometry\": {")
+		print("\"coordinates\": [" + str(way.nodes[0].lat) + ", " + str(way.nodes[0].lon) + "]") #come back to this, not finished
+		print("},")
+		print("\"properties\": {}")
+		print("}")
+		print()
 		
-print(len(returnFootpaths(47.65436866666667, -122.30628233333334,47.65678533333334,  -122.30386566666668)))
+returnFootpaths(47.65436866666667, -122.30628233333334,47.65678533333334,  -122.30386566666668)
 
 
