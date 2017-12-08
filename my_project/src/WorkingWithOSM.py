d10967068e0ce3926db19aea235251a508453b5b
shahy <shahy@localhost.localdomain>
2017-12-08 14:31:47 -0800

    merging

--- my_project/src/WorkingWithOSM.py
+++ my_project/src/WorkingWithOSM.py
@@ -5,7 +5,7 @@ import overpass
 
 def returnFootpaths(slat, slon, nlat, nlon):
 	api = overpy.Overpass()	
-	result = api.query("[bbox: " + str(slat) +"," + str(slon) +"," + str(nlat) + "," + str(nlon) +"]; ( node[highway=footway]; node[highway=pedestrian]; node[foot=yes]; node[footway=sidewalk]; way[highway=footway]; way[highway=pedestrian]; way[foot=yes]; way[footway=sidewalk] ); out;")
-	return len(result.ways)
+	result = api.query("way(47.65552266666666, -122.30997633333334, 47.656489333333326, -122.30900966666667);/*added by auto repair*/(._;>;);/*end of auto repair*/out;")
+	return result.ways[0]
 		
 print(returnFootpaths(47.596238,-122.34512, 47.633268, -122.27097))
