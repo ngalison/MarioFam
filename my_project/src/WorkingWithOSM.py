55c746686126b74eef1f0552ef2238ff39eb8570
shahy <shahy@localhost.localdomain>
2017-12-08 15:43:24 -0800

    almost done

--- my_project/src/WorkingWithOSM.py
+++ my_project/src/WorkingWithOSM.py
@@ -7,15 +7,21 @@ def returnFootpaths(slat, slon, nlat, nlon):
 	api = overpy.Overpass()	
 	result = api.query(" [bbox: " + str(slat) +", " + str(slon) + ", " + str(nlat) + ", " + str(nlon) + "]; (way[highway=footway]; way[highway=pedestrian]; way[foot=yes]; way[footway=sidewalk] ); /*added by auto repair*/ (._;>;); /*end of auto repair*/ out;")
 	footpaths = result.ways
+	print("{")
+	print("\"type\": \"FeatureCollection\",")
+	print("\"features\": [")
 	for way in footpaths:
 		print("{")
 		print("\"type\": \"Feature\",")
 		print("\"geometry\": {")
-		print("\"coordinates\": [" + str(way.nodes[0].lat) + ", " + str(way.nodes[0].lon) + "]") #come back to this, not finished
+		print("\"type\": \"Point\",")
+		print("\"coordinates\": [" + str(way.nodes[0].lon) + ", " + str(way.nodes[0].lat) + "]") #come back to this, not finished
 		print("},")
 		print("\"properties\": {}")
-		print("}")
+		print("},") #take out last one's comma
 		print()
+	print("]")
+	print("}")
 		
 returnFootpaths(47.65436866666667, -122.30628233333334,47.65678533333334,  -122.30386566666668)
 
