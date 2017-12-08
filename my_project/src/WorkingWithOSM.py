b7be53561125a79e06327aad67586fd26b9b51d6
Joelynuw <joelyn@uw.edu>
2017-12-08 15:19:39 -0800

    fixed working with osm to have more filters

--- my_project/src/WorkingWithOSM.py
+++ my_project/src/WorkingWithOSM.py
@@ -5,9 +5,9 @@ import overpass
 
 def returnFootpaths(slat, slon, nlat, nlon):
 	api = overpy.Overpass()	
-	result = api.query("way[highway=footway](" + str(slat) +", " + str(slon) + ", " + str(nlat) + ", " + str(nlon) + ");/*added by auto repair*/(._;>;);/*end of auto repair*/out;")
-	return result.ways[0]
+	result = api.query(" [bbox: " + str(slat) +", " + str(slon) + ", " + str(nlat) + ", " + str(nlon) + "]; (way[highway=footway]; way[highway=pedestrian]; way[foot=yes]; way[footway=sidewalk] ); /*added by auto repair*/ (._;>;); /*end of auto repair*/ out;")
+	return result.ways
 		
-print(returnFootpaths(47.65552266666666, -122.30997633333334, 47.656489333333326, -122.30900966666667))
+print(len(returnFootpaths(47.65436866666667, -122.30628233333334,47.65678533333334,  -122.30386566666668)))
 
 
