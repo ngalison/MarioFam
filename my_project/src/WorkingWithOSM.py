2c71b26554dffe35de396db78777c34bb5d1c7af
Joelynuw <joelyn@uw.edu>
2017-12-08 14:45:23 -0800

    half baked working with osm updates

--- my_project/src/WorkingWithOSM.py
+++ my_project/src/WorkingWithOSM.py
@@ -4,8 +4,8 @@ import overpass
 
 
 def returnFootpaths(slat, slon, nlat, nlon):
-	api = overpy.Overpass()	
-	result = api.query("way(" + str(slat) +", " + str(slon) + ", " + str(nlat) + ", " + str(nlon) + ");/*added by auto repair*/(._;>;);/*end of auto repair*/out;")
+	api = overpass.API()	
+	result = api.WayQuery("way(" + str(slat) +", " + str(slon) + ", " + str(nlat) + ", " + str(nlon) + ");/*added by auto repair*/(._;>;);/*end of auto repair*/out;")
 	return result.ways[0]
 		
 print(returnFootpaths(47.65552266666666, -122.30997633333334, 47.656489333333326, -122.30900966666667))
