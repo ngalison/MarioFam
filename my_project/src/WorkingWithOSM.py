8c82e5d63e43ed78973bd6698e06bfd4879943f6
Joelynuw <joelyn@uw.edu>
2017-12-08 14:34:59 -0800

    updated workingwithosm

--- my_project/src/WorkingWithOSM.py
+++ my_project/src/WorkingWithOSM.py
@@ -5,9 +5,9 @@ import overpass
 
 def returnFootpaths(slat, slon, nlat, nlon):
 	api = overpy.Overpass()	
-	result = api.query("way(47.65552266666666, -122.30997633333334, 47.656489333333326, -122.30900966666667);/*added by auto repair*/(._;>;);/*end of auto repair*/out;")
+	result = api.query("way(" + str(slat) +", " + str(slon) + ", " + str(nlat) + ", " + str(nlon) + ");/*added by auto repair*/(._;>;);/*end of auto repair*/out;")
 	return result.ways[0]
 		
-print(returnFootpaths(47.596238,-122.34512, 47.633268, -122.27097))
+print(returnFootpaths(47.65552266666666, -122.30997633333334, 47.656489333333326, -122.30900966666667))
 
 
