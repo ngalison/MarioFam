0a3b899c057def2cb8ff868e16562f53d6bc0dfc
Joelynuw <joelyn@uw.edu>
2018-01-25 10:22:12 -0800

    WorkingWithOSM has a new method which prints out areas. Check test.txt for an example of it's output.

--- my_project/src/WorkingWithOSM.py
+++ my_project/src/WorkingWithOSM.py
@@ -3,6 +3,7 @@ import overpy
 import overpass
 from boundingbox import BoundingBox
 
+#given a bounding box and a filename, writes a file of the given filename in GeoJson format with all POINTS with pedestrian tags within the bounding box
 def returnFootpathsPoint(bb, filename):
 	slat = bb.lowery;
 	slon = bb.lowerx;
@@ -38,6 +39,7 @@ def returnFootpathsPoint(bb, filename):
 	f.write("}\n")
 
 
+#given a bounding box and a filename, writes a file of the given filename in GeoJson format with all WAYS with pedestrian tags within the bounding box
 def returnFootpathsLineString(bb, filename):
 	slat = bb.lowery;
 	slon = bb.lowerx;
@@ -79,7 +81,50 @@ def returnFootpathsLineString(bb, filename):
 	f.write("]\n")
 	f.write("}\n")
 		
-#returnFootpaths(47.65436866666667, -122.30628233333334,47.65678533333334,  -122.30386566666668)
-#returnFootpaths(BoundingBox(-122.30628233333334, 47.65436866666667, -122.30386566666668, 47.65678533333334))
+#given a bounding box and a filename, writes a file of the given filename in GeoJson format with all RELATIONS/AREAS with pedestrian tags within the bounding box
+def returnFootpathsAreas(bb, filename):
+	slat = bb.lowery;
+	slon = bb.lowerx;
+	nlat = bb.uppery;
+	nlon = bb.upperx;
+	
+	api = overpy.Overpass()	
+	result = api.query(" [bbox: " + str(slat) +", " + str(slon) + ", " + str(nlat) + ", " + str(nlon) + "]; (relation[highway=footway]; relation[highway=pedestrian]; relation[foot=yes]; relation[footway=sidewalk] ); /*added by auto repair*/ (._;>;); /*end of auto repair*/ out;")
+	footpaths = result.ways
+	
+	f = open(filename, "w+")
+	
+	f.write("{\n")
+	
+	f.write("\"type\": \"FeatureCollection\",\n")
+	f.write("\"features\": [\n")
+	wayCount = 0;
+	for way in footpaths:
+		f.write("{\n")
+		f.write("\"type\": \"Feature\",\n")
+		f.write("\"geometry\": {\n")
+		f.write("\"type\": \"Polygon\",\n")
+		f.write("\"coordinates\": [[ ")
+		nodeCount = 0
+		for node in way.nodes:
+			f.write("[" + str(node.lon) + "," + str(node.lat) + "]")
+			if nodeCount != len(way.nodes) - 1:
+				f.write(", ")
+			nodeCount = nodeCount + 1
+		f.write("]]\n")
+		f.write("},\n")
+		f.write("\"properties\": {}\n")
+		if wayCount == len(footpaths) - 1:
+			f.write("}\n") #take out last one's comma
+		else:
+			f.write("},\n")
+		f.write("\n")
+		wayCount = wayCount + 1
+	f.write("]\n")
+	f.write("}\n")
+
+#Below is an example of a call of one of these methods
+#This will write a file called text.txt, returning all pedestrian relations (areas) within red square
+#returnFootpathsAreas(BoundingBox(-122.31070133333334, 47.65479766666667, -122.30828466666668, 47.657214333333336), "test.txt")
 
 
