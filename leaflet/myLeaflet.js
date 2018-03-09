c5965b9725f9bcee934fa2f8b417bc35731f948a
shahy <shahy@uw.edu>
2018-03-09 14:40:57 -0800

    ready to test entire project

--- leaflet/myLeaflet.js
+++ leaflet/myLeaflet.js
@@ -50,7 +50,7 @@ var drumheller = new L.LatLng(47.653739, -122.307744);
 			alert("Invalid walking distance. Please enter a number.")
 			return;
 		}
-		var localserver = "http://35.226.172.28/Project/selectpaths/" + long + "/" + lat + "/" + walkDist
+		var localserver = "http://35.193.244.221/Project/selectpaths/" + long + "/" + lat + "/" + walkDist
 		console.log(localserver)
 		//const GoogleMaps = new Request(here);		
 		// url (required), options (optional)
